8b24b37485631741c21a32dcec1ac34775b5e45f
J.Bae <johnny.bae00@outlook.com>
2022-08-24 18:26:14 -1000

    web app exercise

---
+++ Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public decimal Price { get; set; }
+        public string? Brand { get; set; }
+    }
+}
